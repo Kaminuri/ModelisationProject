2eb1aa3237e16acaf3aaa8601c8174637ea1b8f4
Clement Cattelle <gamisensei@gmail.com>
2015-04-28 22:19:42 +0200

    Syso du contenu

--- src/view/panels/View3D.java
+++ src/view/panels/View3D.java
@@ -34,9 +34,11 @@ public class View3D extends JPanel{
 		double x0 = d.width / 2;
 		double y0 = d.height - 60;
 		double z0 = (x0/2)+y0/2;
+		System.out.println("\n\n\n\n\n");
 		for(int j = 0; j < i.getPoints().size(); j++){
 			listeX[j] = (int)(i.getPoints().get(j).getX()*c.getZoomX() + x0 + c.getTransX());
 			listeY[j] = (int)(i.getPoints().get(j).getY()*c.getZoomY() + y0 + c.getTransY());
+			System.out.println("X > " + i.getPoints().get(j).getX() + " \tY > "+i.getPoints().get(j).getY());
 		}
 		g.drawPolygon(listeX, listeY, i.getPoints().size());
 	}   
