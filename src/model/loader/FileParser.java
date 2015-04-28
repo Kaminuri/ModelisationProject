cb5e2a3591442bf17b19da10a6131de15ae7d986
Clement Cattelle <gamisensei@gmail.com>
2015-04-28 22:20:06 +0200

    Test cube.gts

--- src/model/loader/FileParser.java
+++ src/model/loader/FileParser.java
@@ -15,9 +15,9 @@ import exceptions.ExceptionPoint;
 import exceptions.ExceptionSegment;
 
 public class FileParser {
-
+	private String fileName = "cube.gts";
 	//private String fileName = "sphere20.gts";
-	private String fileName = "x_wing.gts";
+	//private String fileName = "x_wing.gts";
 	//private String fileName = "poney.gts";
 	//private String fileName = "horse.gts";
 	//private String fileName = "space_shuttle.gts";
