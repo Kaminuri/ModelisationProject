ca78ea7d1328bbbbd03dfe10631e1e49fdd53ee6
Clement Cattelle <gamisensei@gmail.com>
2015-05-06 17:39:31 +0200

    Mise en forme, rien d'utile au code

--- src/model/geometric/Point.java
+++ src/model/geometric/Point.java
@@ -7,12 +7,11 @@ public class Point implements Comparable<Point>,Comparator<Point>{
 	private double x, y , z;
 	public Point(double x, double y, double z){
 		this.x = x;
-		this.y=y;
+		this.y = y;
 		this.z = z;
 	}
-	public Point() {
-		// TODO Auto-generated constructor stub
-	}
+	public Point() {}
+	
 	public double getX(){
 		return x;
 	}
