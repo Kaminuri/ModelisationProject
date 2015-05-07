8b68dee4ae3b36f05366ba5f23358ce97bcacac3
Lefebvre Eric <eric.lefebvre59@orange.fr>
2015-05-07 11:02:23 +0200

    Import

--- src/view/frame/tools/ImportFrame.java
+++ src/view/frame/tools/ImportFrame.java
@@ -1,24 +1,34 @@
 package view.frame.tools;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.FileOutputStream;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import BDD.Base;
+
 public class ImportFrame extends JFrame{
-	public ImportFrame(File f){
+	protected Base bdd;
+	public ImportFrame(final File f){
 		JPanel contain = new JPanel();
+		bdd = new Base();
 		//Label
 		JPanel labelContainer = new JPanel();
 		labelContainer.setLayout(new BoxLayout(labelContainer, BoxLayout.Y_AXIS));
-		JTextField name = new JTextField(f.getName());
+		final JTextField name = new JTextField(f.getName());
 		JLabel tag = new JLabel("Tags");
 		JLabel desc = new JLabel("Description");
 		labelContainer.add(name);
@@ -67,8 +77,8 @@ public class ImportFrame extends JFrame{
 				
 			}
 		});
-		JTextField non = new JTextField();
-		JTextField tg = new JTextField();
+		final JTextField non = new JTextField();
+		final JTextField tg = new JTextField();
 		textContainer.add(buttonToChangeFile);
 		textContainer.add(non);
 		textContainer.add(tg);
@@ -77,6 +87,52 @@ public class ImportFrame extends JFrame{
 		JPanel buttonContainer = new JPanel();
 		buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.Y_AXIS));
 		JButton next = new JButton("Next");
+		next.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String tags = non.getText();
+			String nam = name.getText();
+			String des =  tg.getText();
+			String[] tab = tags.split(",");
+			ArrayList<String> tab2 = new ArrayList<String>() ;
+			if(!bdd.estDeja(nam)){
+				File dest = new File("src\\resources\\models\\" + nam + ".gts");
+				copyFile(f, dest);
+				if(!tg.equals(" ")){
+					tab2.add(nam);						
+					if(tab.length >= 3){
+						for(int i = 1; i < tab.length; i++){
+						tab2.add(tab[i-1]);
+						}
+					}
+					else if(tab.length < 3){
+						int i = 1;
+						for(; i < tab.length + 1; i++){
+							tab2.add(tab[i-1]);
+						}
+						for(; i < 4; i++){
+							tab2.add("null");
+						}
+					}
+				}
+				else{
+					tab2.add(nam);
+					for(int i = 1; i < 3; i ++){
+						tab2.add("null");
+					}
+				}
+				bdd.insert(nam, nam + ".gts", tab2.get(0), tab2.get(1), tab2.get(2), tab2.get(3), 0, 0, 0, des);
+				dispose();
+				JOptionPane.showMessageDialog(new JFrame(), "L'importation a été effectuée avec succee");
+			}
+				
+			else {
+				JOptionPane.showMessageDialog(new JFrame(), "Le nom existe deja");
+			}
+							
+		}
+	}); 
 		JButton cancel = new JButton("Cancel");
 		cancel.addMouseListener(new MouseListener() {
 			
@@ -123,5 +179,37 @@ public class ImportFrame extends JFrame{
 	    this.setLocationRelativeTo(null);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setVisible(true);
+	}
+	
+	public static boolean copyFile(File source, File dest){
+		try{
+			// Declaration et ouverture des flux
+			java.io.FileInputStream sourceFile = new java.io.FileInputStream(source);
+	 
+			try{
+				java.io.FileOutputStream destinationFile = null;
+	 
+				try{
+					destinationFile = new FileOutputStream(dest);
+	 
+					// Lecture par segment de 0.5Mo 
+					byte buffer[] = new byte[512 * 1024];
+					int nbLecture;
+	 
+					while ((nbLecture = sourceFile.read(buffer)) != -1){
+						destinationFile.write(buffer, 0, nbLecture);
+					}
+				} finally {
+					destinationFile.close();
+				}
+			} finally {
+				sourceFile.close();
+			}
+		} catch (IOException e){
+			e.printStackTrace();
+			return false; // Erreur
+		}
+	 
+		return true; // Resultat OK  
 	}
 }
