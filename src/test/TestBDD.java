16274479b2907452ef571ebec8352c3a6eb9106f
JLA23 <eric.lefebvre59@orange.fr>
2015-05-26 13:12:07 +0200

    Test BDD

--- src/test/TestBDD.java
+++ src/test/TestBDD.java
@@ -1,13 +1,17 @@
 package test;
 import java.io.File;
+
 import static org.junit.Assert.*;
+
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.HashMap;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
 import BDD.Base;
 
 public class TestBDD {
@@ -46,11 +50,13 @@ public class TestBDD {
 		catch(Exception e){
 			System.out.println(e);
 		}
+		try{bdd.getCon().close();} catch (Exception e){System.out.println(e.getMessage());}
 	}
 	
 	@Test
 	public void testInsert2() {
 		bdd.insert("test2", "test", "test2", "testx", "test2", "test3", 20, 10, 5, "test");
+		bdd.insert("test", "test", "test", "test1", "test2", "test3", 20, 10, 5, "test");
 		
 		try{
 			bdd.setCon(DriverManager.getConnection(bdd.getUrl(), bdd.getNom(), bdd.getMdp()));
@@ -66,6 +72,7 @@ public class TestBDD {
 		catch(Exception e){
 			System.out.println(e);
 		}
+		try{bdd.getCon().close();} catch (Exception e){System.out.println(e.getMessage());}
 	}
 	
 	@Test
@@ -74,29 +81,61 @@ public class TestBDD {
 		String [] tab = new String [1];
 		tab[0] = "test2";
 		HashMap<String, HashMap<String, String>> resultat = bdd.recherche(tab);
-		System.out.println(resultat);
 		assertTrue(resultat.containsKey("test2"));
 	}
 	
 	@Test
 	public void testRecherche2() {
+		bdd.insert("test2", "test2", "test2", "test3", "testx", "test4", 20, 10, 5, "test");
 		String [] tab = new String [2];
 		tab[0] = "test2";
 		tab[1] = "test3";
 		HashMap<String, HashMap<String, String>> resultat = bdd.recherche(tab);
-		System.out.println(resultat);
 		assertTrue(resultat.containsKey("test2"));
 	}
 	
 	@Test
 	public void testRecherche3() {
+		bdd.insert("test2", "test2", "test2", "test3", "testx", "test4", 20, 10, 5, "test");
 		String [] tab = new String [2];
 		tab[0] = "test2";
 		tab[1] = "test6";
 		HashMap<String, HashMap<String, String>> resultat = bdd.recherche(tab);
-		System.out.println(resultat);
 		assertFalse(resultat.containsKey("test2"));
 	}
+	
+	@Test
+	public void testSelect() {
+		bdd.insert("test", "test", "test", "test1", "test2", "test3", 20, 10, 5, "test");
+		bdd.insert("test2", "test2", "test2", "test3", "testx", "test4", 20, 10, 5, "test");
+		HashMap<String, HashMap<String, String>> resultat = bdd.select();
+		assertNotNull(resultat);
+		assertTrue(resultat.containsKey("test"));
+		assertTrue(resultat.containsKey("test2"));
+		assertFalse(resultat.containsKey("test7"));
+	}
+	
+	@Test
+	public void testDelete() {
+		bdd.insert("test", "test", "test", "test1", "test2", "test3", 20, 10, 5, "test");
+		HashMap<String, HashMap<String, String>> resultat = bdd.select();
+		assertTrue(resultat.containsKey("test"));
+		bdd.delete("test");
+		resultat = bdd.select();
+		assertFalse(resultat.containsKey("test"));
+	}
+	
+	@Test
+	public void testEstDeja1(){
+		bdd.insert("test", "test", "test", "test1", "test2", "test3", 20, 10, 5, "test");
+		assertTrue(bdd.estDeja("test"));
+	}
+	
+	@Test
+	public void testEstDeja2(){
+		assertFalse(bdd.estDeja("test"));
+	}
+	
 	
 
 	@After
