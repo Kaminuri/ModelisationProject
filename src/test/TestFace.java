6ce3b0fc7e1437727268b6f539b2b62622abc80d
Clement Cattelle <gamisensei@gmail.com>
2015-05-26 15:00:44 +0200

    Correction pour segment

--- src/test/TestFace.java
+++ src/test/TestFace.java
@@ -16,8 +16,7 @@ public class TestFace {
 		Point[]t = new Point[3];
 		Segment s1 = new Segment(new Point(4,5,7),new Point(7,5,4));
 		Segment s2 = new Segment(new Point(6,8,9),new Point(10,50,40));
-		Segment s3 = new Segment(new Point(1,2,0),new Point(4,8,1));
-		Face f = new Face(s1,s2,s3);
+		Face f = new Face(s1,s2);
 		t[1] = s1.getP1();
 		t[2] = s1.getP2();
 	}
