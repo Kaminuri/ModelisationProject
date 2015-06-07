52acefa7e7b0d0d95090c15145b715b5940bc3c9
Clement Cattelle <gamisensei@gmail.com>
2015-06-07 17:19:20 +0200

    Suppression imports inutiles

--- src/view/panels/OptionPanel.java
+++ src/view/panels/OptionPanel.java
@@ -6,7 +6,6 @@ import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
