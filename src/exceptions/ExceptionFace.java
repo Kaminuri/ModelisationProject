301fc738ea9416571bc25f4f1b3387474e6c3160
Clement Cattelle <gamisensei@gmail.com>
2015-06-07 17:44:59 +0200

    Suppresion warning

--- src/exceptions/ExceptionFace.java
+++ src/exceptions/ExceptionFace.java
@@ -4,6 +4,7 @@ package exceptions;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+@SuppressWarnings("serial")
 public class ExceptionFace extends Exception{
 	
 	public ExceptionFace() {
