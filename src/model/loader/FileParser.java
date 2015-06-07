38db6324382612d1501d7e9ef5ef65a022920098
Clement Cattelle <gamisensei@gmail.com>
2015-06-07 16:28:55 +0200

    Javadoc

--- src/model/loader/FileParser.java
+++ src/model/loader/FileParser.java
@@ -14,11 +14,12 @@ import exceptions.ExceptionFace;
 import exceptions.ExceptionPoint;
 import exceptions.ExceptionSegment;
 
+
 public class FileParser {
 	//private String fileName = "cube.gts";
 	//private String fileName = "sphere5.gts";
-	private String fileName = "x_wing.gts";
-	//private String fileName = "poney.gts";
+	//private String fileName = "x_wing.gts";
+	private String fileName = "poney.gts";
 	//private String fileName = "horse.gts";
 	//private String fileName = "space_shuttle.gts";
 	
@@ -30,6 +31,13 @@ public class FileParser {
 	private ArrayList<Segment> s = new ArrayList<Segment>();
 	private ArrayList<Face> f = new ArrayList<Face>();
 
+	
+	/**
+	 * Verifie que le fichier en lecture existe et est conforme
+	 * @throws ExceptionPoint si il y a une probleme avec les points
+	 * @throws ExceptionSegment si il y a une probleme avec les segments
+	 * @throws ExceptionFace si il y a une probleme avec les faces
+	 */
 	public FileParser() throws ExceptionPoint, ExceptionSegment, ExceptionFace {
 		String sta;
 		String currnt;
@@ -73,7 +81,10 @@ public class FileParser {
 		}
 	}
 	
-	
+	/**
+	 * Determine le nombre de points,de segments et de faces du modele
+	 * @param sta String , premiere ligne du bufferReader
+	 */
 	private void initNbs(String sta) {
 		String words[] = sta.split(" ");
 		nbPoint = Integer.parseInt(words[0]);
@@ -81,6 +92,10 @@ public class FileParser {
 		nbFace = Integer.parseInt(words[2]);
 	}
 	
+	/**
+	 * Determine les points de la figure
+	 * @param currnt la ligne actuelle du bufferReader
+	 */
 	private void initPoints(String currnt) {
 		String words[] = currnt.split(" ");
 		x = Double.parseDouble(words[0]);
@@ -88,12 +103,20 @@ public class FileParser {
 		z = Double.parseDouble(words[2]);
 	}
 	
+	/**
+	 * Determine les segments de la figure
+	 * @param currnt la ligne actuelle du bufferReader
+	 */
 	private void initSegs(String currnt) {
 		String words[] = currnt.split(" ");
 		p1 = p.get(Integer.parseInt(words[0])-1);
 		p2 = p.get(Integer.parseInt(words[1])-1);
 	}
 	
+	/**
+	 * Determine les faces de la figure
+	 * @param currnt la ligne actuelle du bufferReader
+	 */
 	private void initFace(String currnt) {
 		String words[] = currnt.split(" ");
 		s1 = s.get(Integer.parseInt(words[0])-1);
@@ -101,12 +124,26 @@ public class FileParser {
 		s3 = s.get(Integer.parseInt(words[2])-1);
 	}
 	
+	/**
+	 * Renvoie la liste des points de la figure
+	 * @return ArrayList des points de la figure
+	 */
 	public ArrayList<Point> getListPoints(){
 		return p;
 	}
+
+	/**
+	 * Renvoie la liste des segments de la figure
+	 * @return ArrayList des segments de la figure
+	 */
 	public ArrayList<Segment> getListSegments(){
 		return s;
 	}
+	
+	/**
+	 * Renvoie la liste des faces de la figure
+	 * @return ArrayList des faces de la figure
+	 */
 	public ArrayList<Face> getListFaces(){
 		return f;
 	}
