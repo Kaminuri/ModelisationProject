cc251a89accd497833f37444b92153f5c0df9e9c
Lefebvre Eric <eric.lefebvre59@orange.fr>
2015-06-12 22:18:43 +0200

    Import que fichier extension gts

--- src/view/frame/tools/ImportFrame.java
+++ src/view/frame/tools/ImportFrame.java
@@ -12,7 +12,6 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.FileOutputStream;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -23,7 +22,6 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
 import BDD.Base;
 
 @SuppressWarnings("serial")
@@ -40,142 +38,150 @@ public class ImportFrame extends JFrame{
 		chooser.setFileFilter(filter);
 		if (chooser.showOpenDialog(null)== JFileChooser.APPROVE_OPTION) {
 			f = chooser.getSelectedFile();
-			JPanel contain = new JPanel();
-			bdd = new Base("Base.db");
-			//Label
-			JPanel labelContainer = new JPanel();
-			labelContainer.setLayout(new GridLayout(7,1));
-			String nomFile = f.getName().split(".gts")[0];
-			name = new JTextField(nomFile);
-			JLabel tagz = new JLabel("Tags");
-			JLabel desc = new JLabel("Description");
-			labelContainer.add(name);
-			labelContainer.add(tagz);
-			labelContainer.add(desc);
-			
-			//textSetter
-			JPanel textContainer = new JPanel();
-			textContainer.setLayout(new BoxLayout(textContainer, BoxLayout.Y_AXIS));
-			JButton buttonToChangeFile = new JButton("Fichiers");
-			tag = new JTextField();
-			descript = new JTextArea();
-			descript.setPreferredSize(new Dimension(100,100));
-			buttonToChangeFile.addMouseListener(new MouseListener() {
-				
-				@Override
-				public void mouseReleased(MouseEvent arg0) {
-					// TODO Auto-generated method stub
+			if (f.getName().lastIndexOf(".") > 0) {
+			    String ext = f.getName().substring(f.getName().lastIndexOf("."));
+			    if (ext == ".gts") {
+					bdd = new Base("Base.db");
+					//Label
+					JPanel contain = new JPanel();
+					JPanel labelContainer = new JPanel();
+					labelContainer.setLayout(new GridLayout(7,1));
+					String nomFile = f.getName().split(".gts")[0];
+					name = new JTextField(nomFile);
+					JLabel tagz = new JLabel("Tags");
+					JLabel desc = new JLabel("Description");
+					labelContainer.add(name);
+					labelContainer.add(tagz);
+					labelContainer.add(desc);
 					
-				}
-				
-				@Override
-				public void mousePressed(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void mouseExited(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void mouseEntered(MouseEvent arg0) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void mouseClicked(MouseEvent arg0) {
-					dispose();
-					new ImportFrame();
-				}
-			});
-			
-			textContainer.add(buttonToChangeFile);
-			textContainer.add(tag);
-			textContainer.add(descript);
-			
-			//les buttons
-			JPanel buttonContainer = new JPanel();
-			buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.Y_AXIS));
-			JButton next = new JButton("  Next  ");
-			next.addActionListener(new ActionListener() {
-	
-			@Override
-			public void actionPerformed(ActionEvent e){
-				String tags = tag.getText();
-				String nam = name.getText();
-				String des =  descript.getText();
-				String[] tab = tags.split(", ");
-				ArrayList<String> tab2 = new ArrayList<String>() ;
-				if(!bdd.estDeja(nam)){
-					File dest = new File("resources/models/" + nam + ".gts");
-					copyFile(f, dest);
-					if(!descript.equals(" ")){
-						tab2.add(nam);						
-						if(tab.length >= 3){
-							for(int i = 1; i < tab.length; i++){
-							tab2.add(tab[i-1]);
-							}
+					//textSetter
+					JPanel textContainer = new JPanel();
+					textContainer.setLayout(new BoxLayout(textContainer, BoxLayout.Y_AXIS));
+					JButton buttonToChangeFile = new JButton("Fichiers");
+					tag = new JTextField();
+					descript = new JTextArea();
+					descript.setPreferredSize(new Dimension(100,100));
+					buttonToChangeFile.addMouseListener(new MouseListener() {
+						
+						@Override
+						public void mouseReleased(MouseEvent arg0) {
+							// TODO Auto-generated method stub
+							
 						}
-						else if(tab.length < 3){
-							int i = 1;
-							for(; i < tab.length + 1; i++){
-								tab2.add(tab[i-1]);
-							}
-							for(; i < 4; i++){
-								tab2.add("null");
-							}
+						
+						@Override
+						public void mousePressed(MouseEvent arg0) {
+							// TODO Auto-generated method stub
+							
 						}
-					}
-					else{
-						tab2.add(nam);
-						for(int i = 1; i < 3; i ++){
-							tab2.add("null");
+						
+						@Override
+						public void mouseExited(MouseEvent arg0) {
+							// TODO Auto-generated method stub
+							
 						}
-					}
-					Scanner scan = null;
-					try {
-						scan = new Scanner(f);
-					} catch (FileNotFoundException e1) {
-						e1.printStackTrace();
-					}
-					int sommets = Integer.parseInt(scan.next());
-					int segments = Integer.parseInt(scan.next());
-					int faces = Integer.parseInt(scan.next());
-					bdd.insert(nam, nam + ".gts", tab2.get(0), tab2.get(1), tab2.get(2), tab2.get(3), sommets, segments, faces, des);
-					dispose();
-					JOptionPane.showMessageDialog(new JFrame(), "L'importation a ete effectuee avec succes");
-				}
+						
+						@Override
+						public void mouseEntered(MouseEvent arg0) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+						@Override
+						public void mouseClicked(MouseEvent arg0) {
+							dispose();
+							new ImportFrame();
+						}
+					});
 					
-				else {
-					JOptionPane.showMessageDialog(new JFrame(), "Le nom existe deja");
-				}
-								
+					textContainer.add(buttonToChangeFile);
+					textContainer.add(tag);
+					textContainer.add(descript);
+					
+					//les buttons
+					JPanel buttonContainer = new JPanel();
+					buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.Y_AXIS));
+					JButton next = new JButton("  Next  ");
+					next.addActionListener(new ActionListener() {
+			
+					@Override
+					public void actionPerformed(ActionEvent e){
+						String tags = tag.getText();
+						String nam = name.getText();
+						String des =  descript.getText();
+						String[] tab = tags.split(", ");
+						ArrayList<String> tab2 = new ArrayList<String>() ;
+						if(!bdd.estDeja(nam)){
+							File dest = new File("resources/models/" + nam + ".gts");
+							copyFile(f, dest);
+							if(!descript.equals(" ")){
+								tab2.add(nam);						
+								if(tab.length >= 3){
+									for(int i = 1; i < tab.length; i++){
+									tab2.add(tab[i-1]);
+									}
+								}
+								else if(tab.length < 3){
+									int i = 1;
+									for(; i < tab.length + 1; i++){
+										tab2.add(tab[i-1]);
+									}
+									for(; i < 4; i++){
+										tab2.add("null");
+									}
+								}
+							}
+							else{
+								tab2.add(nam);
+								for(int i = 1; i < 3; i ++){
+									tab2.add("null");
+								}
+							}
+							Scanner scan = null;
+							try {
+								scan = new Scanner(f);
+							} catch (FileNotFoundException e1) {
+								e1.printStackTrace();
+							}
+							int sommets = Integer.parseInt(scan.next());
+							int segments = Integer.parseInt(scan.next());
+							int faces = Integer.parseInt(scan.next());
+							bdd.insert(nam, nam + ".gts", tab2.get(0), tab2.get(1), tab2.get(2), tab2.get(3), sommets, segments, faces, des);
+							dispose();
+							JOptionPane.showMessageDialog(new JFrame(), "L'importation a ete effectuee avec succes");
+						}
+							
+						else {
+							JOptionPane.showMessageDialog(new JFrame(), "Le nom existe deja");
+						}
+										
+					}
+				}); 
+					JButton cancel = new JButton("Cancel");
+					cancel.addActionListener(new ActionListener() {
+						
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							dispose();
+							
+						}
+					});
+					buttonContainer.add(next);
+					buttonContainer.add(cancel);
+					
+					contain.add(labelContainer);
+					contain.add(textContainer);
+					contain.add(buttonContainer);
+					getContentPane().add(contain);
+					//this.setResizable(true);
+					this.pack();
+				    this.setLocationRelativeTo(null);
+				    this.setVisible(true);
+			    }
+			    else if (ext != ".gts") {
+				JOptionPane.showMessageDialog(new JFrame(), "Le fichier selectionne n'est pas de type gts");
+			    }
 			}
-		}); 
-			JButton cancel = new JButton("Cancel");
-			cancel.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					dispose();
-					
-				}
-			});
-			buttonContainer.add(next);
-			buttonContainer.add(cancel);
-			
-			contain.add(labelContainer);
-			contain.add(textContainer);
-			contain.add(buttonContainer);
-			getContentPane().add(contain);
-			//this.setResizable(true);
-			this.pack();
-		    this.setLocationRelativeTo(null);
-		    this.setVisible(true);
 		}
 	}
 	
