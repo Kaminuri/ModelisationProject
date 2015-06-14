af7602d086f4425a77b661c637447a8757ed9ab2
Clement Cattelle <gamisensei@gmail.com>
2015-06-14 18:09:16 +0200

    Javadoc

--- src/view/frame/tools/Open.java
+++ src/view/frame/tools/Open.java
@@ -13,39 +13,42 @@ import model.loader.FileParser;
 public class Open extends Panel{
 	protected Item3D item;
 
+	/**
+	 * Met un listener sur le bouton open, avec un Item3D i en parametre
+	 * @param i l'item3D a ouvrir
+	 */
 	public Open(Item3D i){
 		super();
 		this.item = i;
 
 		JButton open = new JButton("Open");
 		panel4.add(open);
-		
+
 		ActionListener listenerOpen = new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg) {
-		    String select = (String)list.getSelectedValue();
-		    if(select == null){
-		    	
-		    }
-		    else{
-		    	dispose();
-		    	String filename = models.get(select).get("adresse");
-		    	try {
-					item.recreateItem(new FileParser(filename));
-				} catch (ExceptionPoint | ExceptionSegment | ExceptionFace e) {
-					
-					e.printStackTrace();
+				String select = (String)list.getSelectedValue();
+				if(select == null){
+
 				}
-		    }
-		    	
+				else{
+					dispose();
+					String filename = models.get(select).get("adresse");
+					try {
+						item.recreateItem(new FileParser(filename));
+					} catch (ExceptionPoint | ExceptionSegment | ExceptionFace e) {
+
+						e.printStackTrace();
+					}
+				}
+
 			}
- 		};
+		};
 		open.addActionListener(listenerOpen);
-		
+
 		pack();
-	    setLocationRelativeTo(null);
-	    setVisible(true);
+		setLocationRelativeTo(null);
+		setVisible(true);
 	}
-	
+
 }
-	
