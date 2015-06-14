cbef2eaa1126adc9a17ecbe4a11976676f51ca86
Clement Cattelle <gamisensei@gmail.com>
2015-06-14 18:14:10 +0200

    Javadoc

--- src/view/frame/tools/InternalFrameOption.java
+++ src/view/frame/tools/InternalFrameOption.java
@@ -8,6 +8,11 @@ import controller.geometric.GeometricController;
 
 @SuppressWarnings("serial")
 public class InternalFrameOption extends JInternalFrame{
+	
+	/**
+	 * Cree une InternalFrameOption permettant de controler un modele
+	 * @param cg Le GeometricController qui permettra de controler le modele auquel il est rattache
+	 */
 	public InternalFrameOption(GeometricController cg){
 		super("",false,true,false, true);
 		OptionPanel op = new OptionPanel(cg);
