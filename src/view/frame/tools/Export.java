8115882709de76f04b4871f23d565f0ffd0820ce
Clement Cattelle <gamisensei@gmail.com>
2015-06-14 18:08:26 +0200

    Javadoc

--- src/view/frame/tools/Export.java
+++ src/view/frame/tools/Export.java
@@ -16,6 +16,9 @@ public class Export extends Panel{
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Met un listener sur le bouton Export
+	 */
 	public Export(){
 		super();
 		JButton export = new JButton("Export");
